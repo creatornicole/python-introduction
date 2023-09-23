1c04862f0c2f8f19005a25f47e1fb3f561081e88
Nicole Gottschall <ngottschall@web.de>
2023-09-23 18:47:52 +0200

    splits asynchronous topic into subtopics

--- elif-statements.py
+++ elif-statements.py
@@ -1,14 +1,3 @@
-# Tell user if they earned more money than they spent
-earned = int(input("How much money did you earn? "))
-spent = int(input("How much money did you spend? "))
-if earned > spent:
-    print("Great! You earned more than you spent. Good job, keep it up!")
-if earned == spent:
-    print("Watch out, you spent as much money as you earned. Better keep it this.")
-if earned < spent:
-    print("You are in a deficit!")
-
-
 # Register for Class
 # Helps students to help them determine what Spanish class they should sign up for
 
